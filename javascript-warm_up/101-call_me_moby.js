c96dec3e4697e6721420dfe46edba5a7bb2980bb
wainoimwangi <wainoi413@gmail.com>
2023-09-06 20:25:47 +0300

    task 6

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Write a function that executes x times a function.
 
-exports.times = function (x, thefunction) {
+exports.times = function (x, theFunction) {
     for (let i = 0; i < x; i++) {
-        return thefunction();
+        theFunction();
     }
 };
