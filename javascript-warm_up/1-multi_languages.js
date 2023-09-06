3b5f7e91a3783bb5278348707d1650a714e12141
wainoimwangi <wainoi413@gmail.com>
2023-09-06 19:33:32 +0300

    task 1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+// scrpt that prints three lines
+
+let firstline = 'C is fun';
+let secondline = 'Python is cool';
+let thirdline = 'Javascript is amazing';
+
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
