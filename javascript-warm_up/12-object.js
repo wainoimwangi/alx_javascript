d8dd26dbcf751126baa8e9872fcfd22b68ff557f
wainoimwangi <wainoi413@gmail.com>
2023-09-06 19:59:26 +0300

    task 3

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Update this script to replace the value 12 with 89:
+
+const myObject = {
+    type: 'object',
+    value: 12
+};
+console.log(myObject);
+/*
+change the value property
+*/
+myObject.value = 89;
+
+console.log(myObject);
