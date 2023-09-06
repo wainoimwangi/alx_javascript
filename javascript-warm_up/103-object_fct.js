e2f708ea81d8c091d7cae1da08185f4fd7f32c61
wainoimwangi <wainoi413@gmail.com>
2023-09-06 20:41:54 +0300

    task 8

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Update this script by adding a new function incr that increments the integer value.
+
+const myObject = {
+    type: 'object',
+    value: 12
+};
+console.log(myObject);
+/*
+YOUR CODE HERE
+*/
+myObject.incr = function () {
+    this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
