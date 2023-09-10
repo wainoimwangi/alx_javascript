356bd22d7ce11b12dcee38a784b2bcb6b765fd77
wainoimwangi <wainoi413@gmail.com>
2023-09-11 00:14:58 +0300

    task 5

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
-import Rectangle from "./4-rectangle.js"
+#!/usr/bin/node
 
-class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle.js') {
     constructor(size) {
         super(size,size);
     }
