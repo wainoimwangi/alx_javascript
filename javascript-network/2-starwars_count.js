b43e34731b678332f36b3ee065c98f9bbb6750f9
wainoimwangi <wainoi413@gmail.com>
2023-09-13 01:16:42 +0300

    task 2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -2,14 +2,25 @@
 const request = require('request');
 
 const url = process.argv[2];
-const parts = [];
 
-request.get(url+ '/'+18, {encoding: 'utf-8'})
-    .on('data', data => {
-        parts.push(data);
-    })
-    .on('complete', () => {
-        response = parts;
-        const count_id = response.films;
-        console.log(count_id);
-    })
+request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error.message);
+      process.exit(1);
+    }
+    if (response.statusCode !== 200) {
+        console.error(`HTTP Error: ${response.statusCode}`);
+        process.exit(1);
+    }
+    const filmData = JSON.parse(body);
+
+    let wedgeAntillesMovies = 0;
+
+    filmData.results.forEach((film) => {
+        if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+          wedgeAntillesMovies++;
+        }
+    });
+
+    console.log(`${wedgeAntillesMovies}`);
+});
