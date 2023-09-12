54942b32afc2ebc7d2638090941ca754646f4a5a
wainoimwangi <wainoi413@gmail.com>
2023-09-13 01:10:51 +0300

    task 2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -10,16 +10,6 @@ request.get(url+ '/'+18, {encoding: 'utf-8'})
     })
     .on('complete', () => {
         response = parts;
-        const count = response.films;
-        console.log(count);
-    })
-    .on('response', response => {
-        if (response.statusCode !== 200) {
-            console.error(`HTTP Error: ${response.statusCode}`);
-            process.exit(1);
-        }
-    })
-    .on('error', error => {
-        console.error('Error:', error.message);
-        process.exit(1);
+        const count_id = response.films;
+        console.log(count_id);
     })
