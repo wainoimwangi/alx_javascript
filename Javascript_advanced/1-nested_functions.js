d38a3640ffaa26bcbc5959a8c448ca80f23abae2
wainoimwangi <wainoi413@gmail.com>
2023-09-15 23:00:04 +0300

    task 1

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const globalVariable = 'Welcome';
+
+function outer() {
+    alert(globalVariable);
+    let course = 'Holberton';
+
+    function inner() {
+        alert(globalVariable + ' ' + course);
+        let exclamation = '!';
+        
+        function inception() {
+            alert(globalVariable + ' ' + course + ' ' + exclamation);
+        }
+        inception();
+    };
+    inner();
+};
+outer();
