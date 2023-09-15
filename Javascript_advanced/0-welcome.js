219ce1878ee9cde5011314d21de8cf208df8b234
wainoimwangi <wainoi413@gmail.com>
2023-09-15 22:39:23 +0300

    task 0

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function welcome(firstName, lastName) {
+    let fullName = firstName + ' ' + lastName;
+
+    function displayFullName () {
+        alert('Welcome' + ' ' + fullName + '!');
+
+    displayFullName();
+    }
+}
