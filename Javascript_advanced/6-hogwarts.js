f931c2cce8d8e8f6b3b89776cd323c22c36d53e9
wainoimwangi <wainoi413@gmail.com>
2023-09-18 11:19:13 +0300

    task 6,7

--- Javascript_advanced/6-hogwarts.js
+++ Javascript_advanced/6-hogwarts.js
@@ -1,12 +1,49 @@
 #!/usr/bin/node
 
-function studentHogwarts () {
+function studentHogwarts(privateScore, name) {
     let privateScore = 0;
     let name = null;
 
-    function changeScoreBy(points) {
-        return points + privateScore;
-    }
+    const changeScoreBy = function (points) {
+        return privateScore += points;
+    };
 
-    f
-}
+    const setName = function (newName) {
+        name = newName;
+    };
+
+    const rewardStudent = function () {
+        changeScoreBy(1);
+    };
+
+    const penalizeStudent = function () {
+        changeScoreBy(-1);
+    };
+
+    const getScore = function () {
+        return `${name}: ${privateScore}`;
+    };
+
+    return {
+        setName,
+        rewardStudent,
+        penalizeStudent,
+        getScore,
+    };
+};
+
+const harry = studentHogwarts();
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+const draco = studentHogwarts();
+draco.setName("Draco");
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
